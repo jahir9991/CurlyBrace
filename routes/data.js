8a5dad97b39199d005daea784edc26b78a059200
JAHIR ALAM RAJU <jahiralamraju@hotmail.com>
2016-03-12 23:52:29 +0600

    first commit

--- routes/data.js
+++ routes/data.js
@@ -1,22 +1,44 @@
 var express = require('express');
 var Firebase = require('firebase');
 var dataRef = new Firebase('https://blood.firebaseio.com/');
-
+var mongoose = require('mongoose');
+    mongoose.connect('mongodb://jahir9991:hambuHAMBU@ds011429.mlab.com:11429/curlybrace');
+var Cat = mongoose.model('Cat', {name: String});
 
 
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    //var d = {
-    //    name: "raju",
-    //    id: "12-20251-1"
-    //}
+
+
+
+
+     //
+     //var kitty = new Cat({name: 'Zildjian'});
+     //
+     //
+     //kitty.save(function (err) {
+     //    if (err) {
+     //        console.log(err);
+     //    } else {
+     //        console.log('meow');
+     //       // res.render('index', {title: kitty});
+     //
+     //
+     //    }
+     //});
+
+    Cat.findById("56e450c284503f9c3b2b4879", function (err, doc){
+        res.json(doc ) ;
+    });
+
+
 
 
     dataRef.child("user").on("value", function (snapshot) {
         var f = snapshot.val();  // Alerts "San Francisco"
-        res.json(f);
+       // res.json(f);
     });
 
 
