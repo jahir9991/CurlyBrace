4fd171daaa59498aa8a570efad36e126fcae24da
JAHIR ALAM RAJU <jahiralamraju@hotmail.com>
2016-03-12 08:36:00 +0600

    first commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CURLY BRACE ' });
+  res.render('index', { title: '{ CURLY { BRACE ' });
 });
 
 module.exports = router;
